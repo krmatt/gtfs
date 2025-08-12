3a2b14d14e5ad9b8c822c2eb091039bf45effb57
Matt Kramer <krmr.mtt@gmail.com>
2025-08-12 09:26:36 -0400

    remove outliers and plot headways

--- mbta/src/stop_frequency_analysis.py
+++ mbta/src/stop_frequency_analysis.py
@@ -1,6 +1,10 @@
 import sqlite3
 
+import matplotlib
+import matplotlib.pyplot as plt
 import pandas as pd
+
+matplotlib.use("Agg")
 
 
 def calculate_headways(db: str, table: str) -> None:
@@ -19,6 +23,20 @@ def calculate_headways(db: str, table: str) -> None:
 
     avg_headways = df.groupby(["stop_id", "route_id"])["headway"].mean().reset_index()
     print(avg_headways)
+
+    df_cleaned = df.groupby(["stop_id", "route_id"], group_keys=False).apply(remove_outliers, include_groups=False)
+    df_cleaned["headway_minutes"] = df_cleaned["headway"].dt.total_seconds() / 60
+    df_cleaned["headway_minutes"].hist(bins=80)
+    plt.axvline(x=15, color="r", lw=1)
+    plt.xlabel("Headway (minutes)")
+    plt.ylabel("Occurrences")
+    plt.title("Headways of Frequent Bus Routes")
+    plt.savefig("stop_hist.pdf")
+
+
+def remove_outliers(group):
+    return group[group["headway"] <= group["headway"].quantile(0.95)]
+
 
 if __name__ == "__main__":
     calculate_headways("stop_events.db", "stop_events")
