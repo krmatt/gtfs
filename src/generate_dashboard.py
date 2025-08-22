fb90c9d04553d6154a27adaacfd9979d8fa874e5
Matt Kramer <krmr.mtt@gmail.com>
2025-08-22 17:31:42 -0400

    update plot formats again

--- src/generate_dashboard.py
+++ src/generate_dashboard.py
@@ -3,6 +3,7 @@ import jinja2
 import os
 import pandas as pd
 import plotly.express as px
+import re
 import sqlite3
 
 import mbta_gtfs
@@ -14,12 +15,31 @@ TEMPLATE_DIR = "../templates"
 OUTPUT_DIR = ".."
 DASHBOARD_HTML_FILENAME = "dashboard.html"
 INDEX_HTML_FILENAME = "index.html"
+STOPS_FILENAME = "../MBTA_GTFS_STATIC_DATA/stops.txt"
 
 FREQUENT_HEADWAY_MINUTES = 15
 MAX_HEADWAY_MINUTES = 120
 
-STOP_ID = "2305"  # TODO make these dynamic (so a user can select which combo to view?)
-ROUTE_ID = "77"
+FREQUENT_BUS_ROUTES = [
+    "1",
+    # "15",
+    # "22",
+    # "23",
+    # "28",
+    # "32",
+    # "39",
+    # "57",
+    "66",
+    # "71",
+    # "73",
+    "77",
+    # "104",
+    "109",
+    # "110",
+    # "111",
+    # "116",
+]  # TODO include other frequent bus routes
+
 
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
@@ -41,32 +61,39 @@ def load_data() -> pd.DataFrame:
 
     return df
 
-    # Filter to a specific stop and route, remove very long headways (likely service breaks)
-    # TODO do filtering in specific plot functions?
-    # df_plot = df[
-    #     (df["stop_id"] == STOP_ID) &
-    #     (df["route_id"] == ROUTE_ID) # TODO use direction to disambiguate shared stops between inbound/outbound
-    #     ]
-    # df_plot.loc[df_plot["headway_minutes"] > MAX_HEADWAY_MINUTES, "headway_minutes"] = pd.NA
-    #
-    # return df_plot
+
+def sort_key(s) -> tuple:
+    # Sort series in the scatter plot by route, inbound/outbound, and stop number
+    num_match = re.match(r"(\d+)", s)
+    num = int(num_match.group(1)) if num_match else float("inf")
+
+    return num, s
 
 
-def make_scatter_plot_headways_at_first_and_last_stops(df: pd.DataFrame, route_id: str, first_last_stop_ids: dict) -> str:
+def make_scatter_plot_headways_at_first_and_last_stops(df: pd.DataFrame) -> str:
+    first_last_stop_ids = mbta_gtfs.get_first_and_last_stop_ids(FREQUENT_BUS_ROUTES)
+    df_stops_info = pd.read_csv(STOPS_FILENAME)
+
     # Prepare data
-    stop_ids = [
-        first_last_stop_ids[route_id][0]["first"],
-        first_last_stop_ids[route_id][0]["last"],
-        first_last_stop_ids[route_id][1]["first"],
-        first_last_stop_ids[route_id][1]["last"]
-    ]
+    stop_ids = []
+    for route_id in FREQUENT_BUS_ROUTES:
+        stop_ids.extend([
+            first_last_stop_ids[route_id][0]["first"],
+            first_last_stop_ids[route_id][0]["last"],
+            first_last_stop_ids[route_id][1]["first"],
+            first_last_stop_ids[route_id][1]["last"]
+        ])
+
+    df = df.merge(df_stops_info[["stop_id", "stop_name"]], on="stop_id", how="left")
+
+    df["series"] = (df["route_id"] + " " +
+                    df["direction_id"].map({0: "Outbound", 1: "Inbound"}) + ": " +
+                    df["stop_name"])
 
     df_plot = df[
-        (df["route_id"] == route_id) &
+        (df["route_id"].isin(FREQUENT_BUS_ROUTES)) &
         (df["stop_id"].isin(stop_ids))
     ]
-
-    df_plot["series"] = df['direction_id'].astype(str) + '_' + df['stop_id'].astype(str)
 
     # Create plot
     fig = px.scatter(
@@ -74,43 +101,48 @@ def make_scatter_plot_headways_at_first_and_last_stops(df: pd.DataFrame, route_i
         x="stop_timestamp",
         y="headway_minutes",
         color="series",
-        labels={"x": "Stop Timestamp", "y": "Headway (minutes)"},
-        title=f"Headways at the First and Last Stops of Route {ROUTE_ID}"
+        category_orders={"series": sorted(df_plot["series"].unique(), key=sort_key)},
+        labels={
+            "stop_timestamp": "Stop Timestamp",
+            "headway_minutes": "Headway (minutes)",
+            "series": "Route, Direction, and Stop"
+        },
+        title=f"Headways at First and Last Stops"
     )
     fig.add_hline(
         y=FREQUENT_HEADWAY_MINUTES,
         line_color="red"
     )
+    fig.update_traces(visible="legendonly")
+    fig.for_each_trace(lambda t: t.update(visible=True) if "1 " in t.name else None)
 
     return fig.to_html(full_html=False, include_plotlyjs="cdn")
 
 
-def make_bar_chart_headways_frequency_threshold(df: pd.DataFrame, route_id: str) -> str:
+def make_bar_chart_headways_frequency_threshold(df: pd.DataFrame) -> str:
     # Prepare data
-    df_route = df[df["route_id"] == route_id].dropna(subset="headway_minutes")
-    total_values = len(df_route["headway_minutes"].values)
+    df_freq = df[df["route_id"].isin(FREQUENT_BUS_ROUTES)].dropna(subset="headway_minutes")
+    df_freq["meets_headway_target"] = df_freq["headway_minutes"] <= FREQUENT_HEADWAY_MINUTES
+
+    headways_meeting_target_by_route = {}
+    for route_id in FREQUENT_BUS_ROUTES:
+        df_route = df_freq[df_freq["route_id"] == route_id]
+        headways_meeting_target_by_route[route_id] = df_route["meets_headway_target"].values.sum() / len(df_route["headway_minutes"].values)
 
     df_plot = pd.DataFrame({
-        "Headway": ["Over 15 min", "At or Under 15 min"],
-        "Proportion of Headways": [
-            (df_route["headway_minutes"].values > FREQUENT_HEADWAY_MINUTES).sum() / total_values,
-            (df_route["headway_minutes"].values <= FREQUENT_HEADWAY_MINUTES).sum() / total_values
-        ]
+        "Route": FREQUENT_BUS_ROUTES,
+        "Proportion of Headways Meeting Target": [headways_meeting_target_by_route[route_id] for route_id in FREQUENT_BUS_ROUTES]
     })
-    df_plot["label"] = df_plot["Proportion of Headways"].apply(lambda x: f"{x * 100:.1f}%")
+    df_plot["label"] = df_plot["Proportion of Headways Meeting Target"].apply(lambda x: f"{x * 100:.1f}%")
 
     # Create plot
     fig = px.bar(
         data_frame=df_plot,
-        x="Proportion of Headways",
-        y="Headway",
+        x="Proportion of Headways Meeting Target",
+        y="Route",
         orientation="h",
-        title=f"How Often do {route_id} Buses Meet the 15 Minute Target?",
-        color="Headway",
-        color_discrete_map={
-            "Over 15 min": "orange",
-            "At or Under 15 min": "blue"
-        },
+        title=f"How Often do Frequent Buses Meet the 15 Minute Headway Target?",
+        color="Route",
         range_x=[0,1],
         text="label"
     )
@@ -126,47 +158,54 @@ def make_bar_chart_headways_frequency_threshold(df: pd.DataFrame, route_id: str)
     return fig.to_html(full_html=False, include_plotlyjs=False)
 
 
-def make_histogram_headways_distribution(df: pd.DataFrame, route_id: str) -> str:
+def make_histogram_headways_distribution(df: pd.DataFrame) -> str:
     # Prepare data
-    df_plot = df[df["route_id"] == route_id].dropna(subset="headway_minutes")
+    df_plot = df[df["route_id"].isin(FREQUENT_BUS_ROUTES)].dropna(subset="headway_minutes")
 
     # Create Plot
     fig = px.histogram(
         data_frame=df_plot,
         x="headway_minutes",
-        labels={"x": "Headway (minutes)", "y": "Occurrences"},
-        title=f"Distribution of Headways on Route {route_id}"
+        color="route_id",
+        opacity=0.7,
+        labels={"headway_minutes": "Headway (minutes)", "route_id": "Route"},
+        title=f"Distribution of Headways"
     )
     fig.add_vline(
         x=FREQUENT_HEADWAY_MINUTES,
         line_color="red"
     )
+    fig.update_layout(
+        barmode="overlay",
+        yaxis_title="Count"
+    )
+    fig.update_traces(visible="legendonly")
+    fig.for_each_trace(lambda t: t.update(visible=True) if t.name == "1" else None)
 
     return fig.to_html(full_html=False, include_plotlyjs=False)
 
 
-def render_dashboard(scatter_plot_headways_first_last_html: str,
-                     bar_chart_headways_frequency_threshold_html: str,
-                     histogram_headways_distribution_html: str) -> None:
-    env=jinja2.Environment(loader=jinja2.FileSystemLoader(TEMPLATE_DIR))
-    template = env.get_template(DASHBOARD_HTML_FILENAME)
+def render_dashboard(scatter_plot_headways_first_last: str,
+                     bar_chart_headways_frequency_threshold: str,
+                     histogram_headways_distribution: str) -> None:
+    template=jinja2.Environment(loader=jinja2.FileSystemLoader(TEMPLATE_DIR)).get_template(DASHBOARD_HTML_FILENAME)
     rendered = template.render(
         updated=datetime.now().strftime("%Y-%m-%d %H:%M"),
-        scatter_plot_headways_first_last=scatter_plot_headways_first_last_html,
-        bar_chart_headways_frequency_threshold=bar_chart_headways_frequency_threshold_html,
-        histogram_headways_distribution=histogram_headways_distribution_html
+        routes=FREQUENT_BUS_ROUTES,
+        scatter_plot_headways_first_last=scatter_plot_headways_first_last,
+        bar_chart_headways_frequency_threshold=bar_chart_headways_frequency_threshold,
+        histogram_headways_distribution=histogram_headways_distribution
     )
     with open(os.path.join(OUTPUT_DIR, INDEX_HTML_FILENAME), "w") as f:
         f.write(rendered)
 
 
 def main() -> None:
-    route_first_last_stop_ids = mbta_gtfs.get_first_and_last_stop_ids([ROUTE_ID])
     df = load_data()
-    scatter_plot_headways_first_last_html = make_scatter_plot_headways_at_first_and_last_stops(df, ROUTE_ID, route_first_last_stop_ids)
-    bar_chart_headways_frequency_threshold_html = make_bar_chart_headways_frequency_threshold(df, ROUTE_ID)
-    histogram_headways_distribution_html = make_histogram_headways_distribution(df, ROUTE_ID)
-    render_dashboard(scatter_plot_headways_first_last_html, bar_chart_headways_frequency_threshold_html, histogram_headways_distribution_html)
+    scatter_plot_headways_first_last = make_scatter_plot_headways_at_first_and_last_stops(df)
+    bar_chart_headways_frequency_threshold = make_bar_chart_headways_frequency_threshold(df)
+    histogram_headways_distribution = make_histogram_headways_distribution(df)
+    render_dashboard(scatter_plot_headways_first_last, bar_chart_headways_frequency_threshold, histogram_headways_distribution)
 
 
 if __name__ == "__main__":
